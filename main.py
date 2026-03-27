a362516d1f717dc3549896e1370b41cc986ba053
Rohit Pai <rohitkalsankpai@gmail.com>
2026-03-27 15:25:19 +0530

    Implemented asynchronous request, using the asyncio library, with asyn & await keywords

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from fastapi import FastAPI
+import asyncio
 
 app = FastAPI()
 
@@ -20,4 +21,18 @@ async def endpoint1():
     '''
     print("hello")
     time.sleep(5)
+    print("bye")
+
+@app.get("/2")
+async def endpoint2():
+    '''This is executed asynchronously, when the requests come from 2 different browsers. 
+    If called twice within the span of 5 seconds, the following steps will be executed in order : 
+    1. print hello ( from first request )
+    2. print hello ( from second request )
+    3. print bye ( 5 seconds after first hello )
+    4. print bye ( 5 seconds after second hello )
+
+    '''
+    print("hello")
+    await asyncio.sleep(5)
     print("bye")
